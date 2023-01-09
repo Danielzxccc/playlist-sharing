dc0bdae43f5c3bd4f3aaac748665fb989a3ca8fd
Danielzxccc <araojodaniel321@gmail.com>
2023-01-09 17:22:16 +0800

    fix css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 // import 'bootstrap/scss/bootstrap.scss'
-import '../dist/index.css'
+import './index.css'
 import axios from 'axios'
 
 axios.defaults.baseURL = 'https://playlist-sharing-api.vercel.app/'
