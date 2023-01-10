fe69b97c034fbcb2ed587e596e62d555445c8640
Danielzxccc <araojodaniel321@gmail.com>
2023-01-10 22:27:41 +0800

    fix cards layout

--- src/components/Playlists/PlaylistCards.jsx
+++ src/components/Playlists/PlaylistCards.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const PlaylistCards = ({ item, index }) => {
   return (
     <div className='col bg-transparent my-2 h-100' key={index}>
-      <div className='bg-light px-3 py-2 rounded'>
+      <div className='bg-light px-3 py-2 rounded' role='button'>
         <div className='ratio ratio-1x1'>
           <img
             src={item.imgsrc === null ? alt : item.imgsrc}
@@ -12,7 +12,7 @@ const PlaylistCards = ({ item, index }) => {
             alt={item.description}
           />
         </div>
-        <div className='card bg-secondary'>
+        <div className='card bg-secondary' style={{ height: 150 }}>
           <div className='card-body'>
             <h5 className='card-title fw-bold'>{item.title}</h5>
             <span className='d-flex'>
