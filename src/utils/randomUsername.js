3449fe0156dc9130b8b86dabc11e51b7ed68019f
Danielzxccc <araojodaniel321@gmail.com>
2023-01-19 08:40:25 +0800

    more nouns

--- src/utils/randomUsername.js
+++ src/utils/randomUsername.js
@@ -7,7 +7,16 @@ export function randomUsername() {
     'Energetic',
     'Funny',
   ]
-  const nouns = ['Ape', 'Bear', 'Cat', 'Dog', 'Elephant', 'Fox']
+  const nouns = [
+    'Ape',
+    'Bear',
+    'Cat',
+    'Dog',
+    'Elephant',
+    'Fox',
+    'Eagle',
+    'Tiger',
+  ]
   const randomAdjIndex = Math.floor(Math.random() * adjectives.length)
   const randomNounIndex = Math.floor(Math.random() * nouns.length)
   return adjectives[randomAdjIndex] + nouns[randomNounIndex]
